c5bc79b184cc1517f113171ea69e51b3a0a8c726
obligation1337 <76268609+obligation1337@users.noreply.github.com>
2021-05-23 22:25:24 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,10 +1,14 @@
 #5/16/2021, first python script - web scrapping weather of my city
+#5/23/2021, added datetime module.
 import requests
+import datetime
 from bs4 import BeautifulSoup
 page = requests.get("https://pogoda.interia.pl/prognoza-dlugoterminowa-poznan,cId,27457") #site to scrap weather data
 soup = BeautifulSoup(page.content, 'html.parser')
+date = datetime.datetime.now()
 
 def weather():
+    print(f'Hi! Today is {date.strftime("%A")}, {date.strftime("%d")} of {date.strftime("%B")}, {date.year}')
     temperature = soup.find('div', class_='weather-currently-temp-strict').get_text()
     weather = input("Choose weather language (PL/ENG): ").upper()
     if weather == str("PL"):
